bf8faea152e54d5503e773d180c4b76e4789f566
lin-codePlus <972606225@qq.com>
2024-06-14 15:54:28 +0800

    音乐服务器

--- onlinemusic/src/main/java/com/example/onlinemusic/config/AppConfig.java
+++ onlinemusic/src/main/java/com/example/onlinemusic/config/AppConfig.java
@@ -3,12 +3,34 @@ package com.example.onlinemusic.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public BCryptPasswordEncoder getBCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 添加拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录之后才能当问其他页面
+        LoginInterceptor loginInterceptor = new LoginInterceptor();
+
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**") // 拦截所有路径
+                .excludePathPatterns("/js/**.js") // 除了js文件
+                .excludePathPatterns("/images/**") // 除了图片文件
+                .excludePathPatterns("/css/**.css") // 除了css文件
+                .excludePathPatterns("/fonts/**") // 除了fonts文件夹下的文件
+                .excludePathPatterns("/player/**") // 除了player文件夹下的文件
+                .excludePathPatterns("/login.html") // 除了登录页面
+                .excludePathPatterns("/user/login"); // 除了登录接口
     }
 }
---
+++ onlinemusic/src/main/java/com/example/onlinemusic/config/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package com.example.onlinemusic.config;
+
+import com.example.onlinemusic.tools.Constant;
+import com.example.onlinemusic.tools.ResponseBodyMessage;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+// 设置登录拦截器
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 检测是否登录
+        HttpSession httpSession = request.getSession(false);
+        // session为空 或 session中的属性不存在
+        if (httpSession == null || httpSession.getAttribute(Constant.USERINFO_SESSION_KEY) == null) {
+            System.out.println("未登录，请进行登录！");
+            return false;
+        }
+        return true;
+    }
+}
